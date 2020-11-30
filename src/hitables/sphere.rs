b46e240550fa851db3f8956bdce341608d829098
Krenodeno <thomasfromont.info@gmail.com>
2020-11-30 19:06:04 +0100

    fix sphere uv

--- src/hitables/sphere.rs
+++ src/hitables/sphere.rs
@@ -1,6 +1,7 @@
 use super::hitable::*;
 use super::Aabb;
 use crate::materials::Material;
+use std::f64::consts::PI;
 use std::sync::Arc;
 
 pub struct Sphere {
@@ -10,11 +11,16 @@ pub struct Sphere {
 }
 
 impl Sphere {
+    /// Get the u,v pair of coordinates of the point p on the sphere.
+    /// p: a given pont on the sphere of radius one, centered at the origin.
+    /// u: returned value [0,1] of angle around the Y axis from X = -1.
+    /// v: returned value [0,1] of angle from Y = -1 to Y = +1.
     pub fn get_sphere_uv(p: &Vec3) -> (f64, f64) {
-        let phi = p.z.atan2(p.x);
-        let theta = p.y.asin();
-        let u = 1.0_f64 - (phi + std::f64::consts::PI) / 2.0_f64 * std::f64::consts::PI;
-        let v = (theta + std::f64::consts::PI / 2.0_f64) / std::f64::consts::PI;
+        let phi = (-p.z).atan2(p.x) + PI;
+        let theta = p.y.acos();
+
+        let u = phi / (2.0_f64 * PI);
+        let v = theta / PI;
         (u, v)
     }
 }
